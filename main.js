ec13af5bf05e046330b0f42a64a71e0c1448a779
Teitei Wisdom <120546016+Ajinormotor@users.noreply.github.com>
2024-01-27 01:03:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("header__wrapper");
+
+
+function showMenu() {
+header.style.left = "0px";
+}
+
+function hideMenu() {
+    header.style.left = "-400px";
+   
+    }
